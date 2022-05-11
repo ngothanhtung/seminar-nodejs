638a7568fed62865b8fc4455f099673ccf511325
Ngo Thanh Tung <tungnt@softech.vn>
2022-05-11 07:31:24 +0700

    Update

---
+++ playwright-testing/cypress/integration/tests/test.spec.js
@@ -0,0 +1,14 @@
+const { test, expect } = require('@playwright/test');
+
+test.describe('Basic tests', () => {
+  test('Basic test 1', async ({ page }) => {
+    await page.goto('https://aptech-danang.edu.vn/');
+    await expect(page).toHaveTitle(/Softech1 Aptech2/);
+  });
+
+  test('Basic test 2', async ({ page }) => {
+    await page.goto('https://aptech-danang.edu.vn/chuong-trinh-dao-tao/khoa-ngan-han/fullstack-web-reactjs-nodejs');
+    const title = page.locator('title').first();
+    await expect(title).toContainText('REACTJS & NODEJS');
+  });
+});
