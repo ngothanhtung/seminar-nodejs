b34003b27e3fc038d7e37c2a6553ecdb433e36c7
Ngo Thanh Tung <tungnt@softech.vn>
2022-05-27 19:03:40 +0700

    Update

--- playwright-testing/tests/test.spec.js
+++ playwright-testing/tests/test.spec.js
@@ -7,8 +7,8 @@ test.describe('Basic tests', () => {
   });
 
   test('Basic test 2', async ({ page }) => {
-    await page.goto('https://aptech-danang.edu.vn/chuong-trinh-dao-tao/khoa-ngan-han/fullstack-web-reactjs-nodejs');
+    await page.goto('https://aptech-danang.edu.vn');
     const title = page.locator('title').first();
-    await expect(title).toContainText('REACTJS & NODEJS');
+    await expect(title).toContainText('APTECH');
   });
 });
